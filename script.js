d3d3d573f642bcf3b58a0a60d1e3d657906c8fb6
jonathanreedmevs <jrmevs@gmail.com>
2020-02-05 08:05:44 -0500

    change window location

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 $('#3A').click(function(){
-  // window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html"
+  window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html"
 
   //below is for the pc
   //window.location.href = "file:///C:/Users/Jonathan/Documents/GitHub/RandomStudent/random3.html"
 
   //below is for the mac
-  window.location.href= "/Users/jonathanmevs/Documents/GitHub/RandomStudent/random3.html"
+  // window.location.href= "/Users/jonathanmevs/Documents/GitHub/RandomStudent/random3.html"
 })
 
 $('#add').click(function(){
+
+  window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html
 
   //below is for the pc
   //window.location.href = "file:///C:/Users/Jonathan/Documents/GitHub/RandomStudent/add_student.html"
 
   //below is for the mac
-  window.location.href = "/Users/jonathanmevs/Documents/GitHub/RandomStudent/add_student.html"
+  //window.location.href = "/Users/jonathanmevs/Documents/GitHub/RandomStudent/add_student.html"
 })
