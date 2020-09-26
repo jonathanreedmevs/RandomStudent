3b302861405b263bc6e4efdc79bd9148e4687874
jonathanreedmevs <jrmevs@gmail.com>
2020-09-26 14:58:44 -0400

    -incorporated courses for '20-'21 school year -improved homescreen layout -improved add functionality

--- add.js
+++ add.js
@@ -1,36 +1,60 @@
-const STU_LIST_3 = fire.collection("3A Students").doc("Student Lists");
-const STU_LIST_4 = fire.collection("4A Students").doc("Student Lists");
-function addStudent3(){
+const STU_LIST_3A = fire.collection("3A Students").doc("Student Lists");
+const STU_LIST_4A = fire.collection("4A Students").doc("Student Lists");
+const STU_LIST_3B = fire.collection("3B Students").doc("Student Lists");
+const STU_LIST_4B = fire.collection("4B Students").doc("Student Lists");
+
+function addStudent(period) {
+  console.log("hey");
   let newStudent = {
     name: $('#nameInput').val(),
     pic_src: $('#picSrcInput').val(),
-    score: [0,0,0,0]
+    score: [0, 0, 0, 0]
   };
-  // let lst = [];
-  // lst.push(newStudent);
-  STU_LIST_3.update({
-    to_ask_pool: firebase.firestore.FieldValue.arrayUnion(newStudent)
-  });
+
+  switch (period) {
+    case "3A":
+      STU_LIST_3A.update({
+        to_ask_pool: firebase.firestore.FieldValue.arrayUnion(newStudent)
+      });
+      break;
+
+    case "4A":
+      STU_LIST_4A.update({
+        to_ask_pool: firebase.firestore.FieldValue.arrayUnion(newStudent)
+      });
+      break;
+
+    case "3B":
+      STU_LIST_3B.update({
+        to_ask_pool: firebase.firestore.FieldValue.arrayUnion(newStudent)
+      });
+      break;
+
+    case "4B":
+      STU_LIST_4B.update({
+        to_ask_pool: firebase.firestore.FieldValue.arrayUnion(newStudent)
+      });
+      break;
+  }
 
   $("#nameInput").val('');
   $("#picSrcInput").val('');
 }
 
-function addStudent4(){
-  let newStudent = {
-    name: $('#nameInput').val(),
-    pic_src: $('#picSrcInput').val(),
-    score: [0,0,0,0]
-  };
-  // let lst = [];
-  // lst.push(newStudent);
-  STU_LIST_4.update({
-    to_ask_pool: firebase.firestore.FieldValue.arrayUnion(newStudent)
-  });
 
-  $("#nameInput").val('');
-  $("#picSrcInput").val('');
-}
-
-$('#add3').click(addStudent3);
-$('#add4').click(addStudent4);
+$('#add3A').click(function(event) {
+  event.preventDefault();
+  addStudent("3A")
+});
+$('#add4A').click(function(event) {
+  event.preventDefault();
+  addStudent("4A")
+});
+$('#add3B').click(function(event) {
+  event.preventDefault();
+  addStudent("3B")
+});
+$('#add4B').click(function(event) {
+  event.preventDefault();
+  addStudent("4B")
+});
--- script.js
+++ script.js
@@ -2,10 +2,10 @@ $('#3A').click(function(){
   //window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html"
 
   //below is for the pc
-  //window.location.href = "file:///C:/Users/Jonathan/Documents/GitHub/RandomStudent/random3.html"
+  window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/random3.html"
 
   //below is for the mac
-  window.location.href= "/Users/jonathanmevs/Documents/GitHub/RandomStudent/random3.html"
+  //window.location.href= "/Users/jonathanmevs/Documents/GitHub/RandomStudent/random3.html"
 })
 
 $('#add').click(function(){
@@ -13,12 +13,13 @@ $('#add').click(function(){
   //window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html"
 
   //below is for the pc
-  //window.location.href = "file:///C:/Users/Jonathan/Documents/GitHub/RandomStudent/add_student.html"
+  window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/add_student.html"
 
   //below is for the mac
-  window.location.href = "/Users/jonathanmevs/Documents/GitHub/RandomStudent/add_student.html"
+  //window.location.href = "/Users/jonathanmevs/Documents/GitHub/RandomStudent/add_student.html"
 })
 
 $('#4A').click(function(){
-  window.location.href = "/Users/jonathanmevs/Documents/GitHub/RandomStudent/random4.html";
+  //pc
+  window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/random4.html";
 })
