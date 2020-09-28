5fbb194fc50a86c47467c60b5f9280fe044e7b36
jonathanreedmevs <jrmevs@gmail.com>
2020-09-28 11:14:24 -0400

    Renamed files

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-$('#3A').click(function(){
-  //window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html"
+$('#3A').click(function() {
+  window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3A.html"
 
   //below is for the pc
-  window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/random3.html"
+  //window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/random3.html"
 
   //below is for the mac
   //window.location.href= "/Users/jonathanmevs/Documents/GitHub/RandomStudent/random3.html"
 })
 
-$('#add').click(function(){
+$('#add').click(function() {
 
-  //window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random3.html"
+  window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/add_student.html"
 
   //below is for the pc
-  window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/add_student.html"
+  //window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/add_student.html"
 
   //below is for the mac
   //window.location.href = "/Users/jonathanmevs/Documents/GitHub/RandomStudent/add_student.html"
 })
 
-$('#4A').click(function(){
+$('#4A').click(function() {
   //pc
-  window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/random4.html";
+  //window.location.href = "file:///C:/Users/Jonathan/github/RandomStudent/random4.html";
+
+  window.location.href = "https://jonathanreedmevs.github.io/RandomStudent/random4A.html"
 })
